9f0383aeb2d906f783549e5bc07d16712659974a
slowlife7 <rasgo@naver.com>
2019-10-14 10:53:52 +0900

    스트리밍 함수 추가

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
-const indexRouter = require('./route/index');
-const mobileRouter = require('./route/mobile');
-const videoRouter = require('./route/video');
-const loginRouter = require('./route/login');
+const indexRouter = require("./route/index");
+const mobileRouter = require("./route/mobile");
+const videoRouter = require("./route/video");
+const loginRouter = require("./route/login");
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rasgo.iptime.org:27017/toapp6', {useNewUrlParser: true})
-.then( () => { console.log('=====>Succeeded in connecting..'); } )
-.catch( (err) => { console.log(err); })
-
+mongoose
+  .connect("mongodb://rasgo.iptime.org:27017/toapp6", { useNewUrlParser: true })
+  .then(() => {
+    console.log("=====>Succeeded in connecting..");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('*.ico', FaviconHandler);
-
-
-app.use('/video', videoRouter);
-app.use('/test/test1', loginRouter);
-app.use('/', indexRouter);
-app.use('/mobile/test', mobileRouter);
+app.use("*.ico", FaviconHandler);
+app.use("/video", videoRouter);
+app.use("/test/test1", loginRouter);
+app.use("/", indexRouter);
+app.use("/mobile/test", mobileRouter);
 app.use(ErrorHandler);
 app.use(CatchError);
 
 app.listen(3000, function() {
-	console.log('app listening on port 3000!');
+  console.log("app listening on port 3000!");
 });
 
-
 function FaviconHandler(req, res, next) {
-  console.log('favicon');
+  console.log("favicon");
   console.log(req.path);
   res.status(200).end();
 }
 
 function ErrorHandler(req, res, next) {
-	console.log('Can not found any router!!!!');
-	res.status(404).end();
+  console.log("Can not found any router!!!!");
+  res.status(404).end();
 }
 
 function CatchError(err, req, res, next) {
-	console.log('Can not found any router');
-	res.status(500).end();
+  console.log("Can not found any router");
+  res.status(500).end();
 }
-
--- route/video.js
+++ route/video.js
@@ -3,13 +3,78 @@ const Video = require("../model/video");
 const router = express.Router();
 const boardAPI = require("../private/javascript/boardAPI")(Video);
 
+router.get("/*.mp4", function(req, res, next) {
+  const file = "/home/bear/video" + decodeURI(req.path);
+  console.log(file);
+  if (!fs.existsSync(file)) {
+    return;
+  }
+
+  fs.stat(file, (err, stat) => {
+    if (err) {
+      //not found file
+      if (err.code === "ENOENT") {
+        return;
+      }
+    }
+
+    let start = 0,
+      end = stat.size,
+      total = 0,
+      contentLength = stat.size;
+    let contentRange = false;
+    const range = req.header("Range");
+    if (range) {
+      const position = range.replace(/bytes=/, "").split("-");
+      start = parseInt(position[0], 10);
+      total = stat.size;
+      end = position[1] ? parseInt(position[1], 10) : total - 1;
+      contentRange = true;
+      contentLength = end - start + 1;
+    }
+
+    if (start <= end) {
+      if (contentRange) {
+        res.status(206).set({
+          "Accept-Ranges": "bytes",
+          "Content-Length": contentLength,
+          "Content-Type": "video/mp4",
+          "Content-Range": "bytes " + start + "-" + end + "/" + total
+        });
+      } else {
+        res.status(200).set({
+          "Accept-Ranges": "bytes",
+          "Content-Length": contentLength,
+          "Content-Type": "video/mp4"
+        });
+      }
+      let st = fs
+        .createReadStream(file, { start: start, end: end })
+        .on("end", function() {
+          console.log("Stream done");
+          res.end();
+        })
+        .on("error", function(err) {
+          console.error(err);
+          res.end();
+        })
+        .pipe(
+          res,
+          { end: true }
+        );
+    } else {
+      res.status(403).end();
+    }
+  });
+});
+
 router.get(
   ["/", "/:title"],
   (req, res, next) => {
     let page_no = 0,
       count_pagination = 5,
       count_per_page = 10;
-      //refresh = false;
+    //refresh = false;
 
     const query = req.query;
     if (Object.keys(query).length) {
@@ -33,7 +98,7 @@ router.get(
       condition: {},
       sort: { created: -1 },
       skip: req.page_no * req.count_per_page,
-      limit: parseInt(req.count_per_page),
+      limit: parseInt(req.count_per_page)
     };
 
     boardAPI
@@ -57,7 +122,7 @@ router.get(
       condition: { title: { $regex: ".*" + req.params.title + ".*" } },
       sort: { created: -1 },
       skip: req.page_no * req.count_per_page,
-      limit: parseInt(req.count_per_page),
+      limit: parseInt(req.count_per_page)
     };
 
     boardAPI
@@ -80,7 +145,8 @@ router.get(
 
 function getPageInfo(count_items, count_per_page, count_pagination, page_no) {
   const count_page_group = Math.ceil(count_items / count_per_page); //페이징으로 보여질 총 페이지 개수
-  let last_page_group =Math.ceil(page_no / count_pagination) * count_pagination; //현재 페이징에서 가장 마지막 페이지
+  let last_page_group =
+    Math.ceil(page_no / count_pagination) * count_pagination; //현재 페이징에서 가장 마지막 페이지
   const first_page_group = last_page_group - count_pagination + 1; //현재 페이징에서 가장 첫 페이지
   if (last_page_group > count_page_group) {
     // 가장 마지막페이지가 전체 페이지 개수 보다 큰 경우
